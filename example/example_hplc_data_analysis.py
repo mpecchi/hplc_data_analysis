4c17429f80fc3d7106afb21cc05d8b105a8c5b2d
mpecchi <matteo.pecchi@gmail.com>
2024-05-12 15:27:21 -0400

    updated examples and gitignore

--- example/example_hplc_data_analysis.py
+++ example/example_hplc_data_analysis.py
@@ -1,64 +1,82 @@
-# example of hplc_data_analysis
-import pathlib as plib
-from hplc_data_analysis import Project
+# %%
+from hplc_data_analysis.hplc import Project
+
 # this has to be changed to where the _example/data folder is
-folder_path = plib.Path(plib.Path.cwd().parent, 'example/data')
+folder_path = r"/Users/matteo/Projects/hplc_data_analysis/example/data"
 # folder_path = plib.Path(plib.Path(__file__).cwd(), 'data')
 # change this to folder_path = plib.Path(r"C:\Path\To\Your\Data") for your project
 # class methods need to be called at the beginning to influence all instances
-Project.set_folder_path(folder_path)
-Project.set_plot_grid(False)
-Project.set_plot_font('Sans')  # ('Times New Roman')
 
-p = Project(rebuild_compounds_properties=False)
+hplc = Project(folder_path)
 # %%
-# p.create_compounds_properties()
-# get files, replicates, and samples info
-files_info = p.files_info
-replicates_info = p.replicates_info
-samples_info = p.samples_info
-# create files, replicates and samples dfs
-p.create_files_replicates_samples()
-#%%
-# example of file
-FWCP250C1h1_1_210 = p.files['210_FWCP250C1h1_1']
-# examples of replicates (merged files with diff. methods)
-FWCP250C1h1_1 = p.replicates['FWCP250C1h1_1']
-FWCP250C1h1_2 = p.replicates['FWCP250C1h1_2']
-# examples of samples (average of replicates)
-FWCP250C1h1 = p.samples['FWCP250C1h1']
-FWCP250C1h1_std = p.samples_std['FWCP250C1h1']
-# add all statistics to info dfs
-p.update_all_info_statistics()
-# create report and aggrreports for different parameters
-# for files, replicates and samples
-files_report_conc = \
-    p.create_files_param_report(param='conc_vial_if_undiluted_mg_L')
-replicates_report_conc = \
-    p.create_replicates_param_report(param='conc_vial_if_undiluted_mg_L')
-replicates_report_fr = \
-    p.create_replicates_param_report(param='fraction_of_sample_fr')
-replicates_aggrrep_fr = \
-    p.create_replicates_param_aggrrep(param='fraction_of_sample_fr')
-samples_report_fr, samples_report_fr_std = \
-    p.create_samples_param_report(param='fraction_of_sample_fr')
-samples_aggrrep_fr, samples_report_fr_std = \
-    p.create_samples_param_aggrrep(param='fraction_of_sample_fr')
-p.save_files_info()
-p.save_replicates_info()
-p.save_samples_param_report()
-p.save_samples_param_aggrrep()
+# create files info (dilution and total sample concentration information need to be added manually)
+files_info_created = hplc.create_files_info()
+# %% load the files info with dilution data and concentration
+files_info = hplc.load_files_info(update_saved_files_info=False)
 # %%
-p.plot_ave_std(param='conc_vial_mg_L', aggr=True, min_y_thresh=0,
-    y_lim=[0, 50000],legend_location='outside',
-    color_palette='Set2')
+# create replicates and samples info
+replicates_info = hplc.create_replicates_info()
+samples_info = hplc.create_samples_info()
+# create the samples (intended as multiple replicates (multiple files))
+hplc.create_samples()
 # %%
-p.plot_ave_std(param='conc_vial_if_undiluted_mg_L', replicates_or_samples='replicates',
-    min_y_thresh=0, legend_location='outside', xlab_rot=20,
-    # only_samples_to_plot=['FW250C1h1', 'FWCP250C1h1'],
-    y_lim=[0, 50000], annotate_outliers=True)
+# to get a list of all files, replicates and samples
+list_of_all_filenames = files_info.index.tolist()
+list_of_all_replicatenames = replicates_info.index.tolist()
+list_of_all_samplenames = samples_info.index.tolist()
 # %%
-p.plot_ave_std(replicates_or_samples='samples', param='fraction_of_sample_fr',
-    aggr=True, min_y_thresh=2000, legend_location='outside', xlab_rot=20,
-    # only_samples_to_plot=['FW250C1h1', 'FWCP250C1h1'],
-    y_lim=[0, 50000], annotate_outliers=True)
+# plot the chosen param for all files, names_to_keep is a list of the files to plot
+mf = hplc.plot_report(
+    files_replicates_or_samples="files",
+    param="conc_vial_mg_L",
+    names_to_keep=[
+        "210_FW250C1h1_1",
+        # "210_FW250C1h1_2",
+        "210_FWCP250C1h1_1",
+        "210_FWCP250C1h1_2",
+        "254_FW250C1h1_1",
+        "254_FW250C1h1_2",
+        "254_FWCP250C1h1_1",
+        "254_FWCP250C1h1_2",
+        # "254_FWCPMn250C1h1_1",
+    ],
+    height=5,
+    width=10,
+    x_ticklabels_rotation=20,
+    legend_ncols=2,
+)
+# %%
+# plot replicates, which are the combination of files with
+# different methods but same vial
+mf = hplc.plot_report(
+    files_replicates_or_samples="replicates",
+    param="conc_vial_mg_L",
+    names_to_keep=[
+        "FW250C1h1_1",
+        "FW250C1h1_2",
+        "FWCP250C1h1_1",
+        "FWCP250C1h1_2",
+    ],
+    y_axis_min_threshold=10000,
+    height=5,
+    width=10,
+    x_ticklabels_rotation=20,
+    legend_ncols=1,
+)
+# %%
+# plot samples, which are the combination of replicates for the same
+# material
+mf = hplc.plot_report(
+    files_replicates_or_samples="samples",
+    param="conc_vial_mg_L",
+    names_to_keep=["FW250C1h1", "FWCP250C1h1", "FWCPMn250C1h1"],
+    y_axis_min_threshold=10000,
+    x_ticklabels_rotation=20,
+    legend_ncols=1,
+)
+
+# %%
+
+
+for level, param in zip(["files", "repliactes", "samples"], hplc.acceptable_params):
+    hplc.plot_report(r)
